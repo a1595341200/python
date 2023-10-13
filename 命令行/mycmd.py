4356e065d8e53b1240e5b821901f4579e06eb56f
谢瑶 <>
2023-10-13 15:59:29 +0800

    修改cmd类输出去掉总输出

--- 命令行/mycmd.py
+++ 命令行/mycmd.py
@@ -1,3 +1,11 @@
+'''
+Author: 谢瑶 
+Date: 2022-09-29 11:58:13
+LastEditors: 谢瑶 
+LastEditTime: 2023-10-13 15:57:49
+FilePath: /python/命令行/mycmd.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import subprocess
 
 
@@ -15,4 +23,5 @@ class Cmd:
             for i in iter(ret.stdout.readline, b""):
                 print(i.decode().strip())
                 l.append(i.decode().strip())
-            print("finally------>", l)
+            # print("finally------>", l)
+            print(self.cmd, " 执行完毕")
