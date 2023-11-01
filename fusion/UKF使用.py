18bccfb8b9dbd5b57e0ea36bf82aeec91a4a1c94
谢瑶 <>
2023-11-01 10:13:51 +0800

    ukf

--- fusion/UKF使用.py
+++ fusion/UKF使用.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from filterpy.common import Q_discrete_white_noise
+import filterpy.common
 from filterpy.kalman import MerweScaledSigmaPoints
 from filterpy.kalman import UnscentedKalmanFilter
 from numpy.random import randn
@@ -27,13 +27,16 @@ kf.x = np.array([-1., 1., -1, -1])  # initial state
 kf.P *= 0.2  # initial uncertainty
 z_std = 0.1
 kf.R = np.diag([z_std ** 2, z_std ** 2])  # 1 standard
-kf.Q = Q_discrete_white_noise(dim=2, dt=dt, var=0.01 ** 2, block_size=2)
+kf.Q = filterpy.common.Q_discrete_white_noise(dim=2, dt=dt, var=0.01 ** 2, block_size=2)
 
-zs = [[i + randn() * z_std, i + randn() * z_std] for i in range(50)]  # measurements
+zs = [[i + randn() * z_std, i + randn() * z_std] for i in range(500)]  # measurements
+realz = [[i, i] for i in range(500)]
+
 resx = []
 resy = []
 reszx = []
 reszy = []
+
 # print(zs)
 for z in zs:
     kf.predict()
@@ -42,6 +45,13 @@ for z in zs:
     resy.append(kf.x_post[2])
     reszx.append(z[0])
     reszy.append(z[1])
+plt.subplot(1, 3, 1)
 plt.plot(resx, resy)
 plt.plot(reszx, reszy)
+ex = [[resx[i] - realz[i][0]] for i in range(len(resx))]
+ey = [[resy[i] - realz[i][1]] for i in range(len(resy))]
+plt.subplot(1, 3, 2)
+plt.plot(reszx, ex)
+plt.subplot(1, 3, 3)
+plt.plot(reszy, ey)
 plt.show()
